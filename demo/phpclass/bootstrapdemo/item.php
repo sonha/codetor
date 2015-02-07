53d4d1e4a891b7b13a38c891ac65cc65fdc2bc52
sonha <sonha@users.noreply.github.com>
2015-02-07 15:04:21 +0700

    Add font Awesome

--- demo/phpclass/bootstrapdemo/item.php
+++ demo/phpclass/bootstrapdemo/item.php
@@ -83,28 +83,11 @@ require_once 'listProduct.php';
 
                 <div class="caption-full">
                     <h4 class="pull-right">$24.99</h4>
-                    <h4><a href="#">Product Name</a>
-                    </h4>
-
-                    <p>See more snippets like these online store reviews at <a target="_blank"
-                                                                               href="http://bootsnipp.com">Bootsnipp -
-                            http://bootsnipp.com</a>.</p>
-
-                    <p>Want to make these reviews work? Check out
-                        <strong><a
-                                href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this
-                                building a review system tutorial</a>
-                        </strong>over at maxoffsky.com!</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <h4><a href="#">Product Name</a></h4>
+                    Mô tả sản phẩm ở đây
                 </div>
                 <div class="ratings">
                     <p class="pull-right">3 reviews</p>
-
                     <p>
                         <span class="glyphicon glyphicon-star"></span>
                         <span class="glyphicon glyphicon-star"></span>
@@ -117,13 +100,11 @@ require_once 'listProduct.php';
             </div>
 
             <div class="well">
-
                 <div class="text-right">
                     <a class="btn btn-success">Leave a Review</a>
                 </div>
 
                 <hr>
-
                 <div class="row">
                     <div class="col-md-12">
                         <span class="glyphicon glyphicon-star"></span>
